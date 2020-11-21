49ab0f5c31322c64ed1342538ffd44a3c98a1f43
rayan4444 <rayan.armani@gmail.com>
2020-11-21 16:45:31 +0100

    Create CheckMatrix.m
    check matrix from patpat

---
+++ CheckMatrix.m
@@ -0,0 +1,16 @@
+
+%Open the example matrices
+P_ex= load('example_P');
+P_array = struct2array(P_ex);
+
+
+G_ex = load('example_G');
+G_array =  struct2array(G_ex);
+
+%Self-Computed P and G:
+P=ComputeTransitionProbabilities(stateSpace, map);
+G = ComputeStageCosts(stateSpace, map);
+
+%Checks if both matrices are the same (output 0 if no, 1 if yes)
+P_check= isequal(P, P_array)
+G_check = isequal(G, G_ex);
